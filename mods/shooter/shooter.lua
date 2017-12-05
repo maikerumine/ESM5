f2cb4708d0cfaa1af2b56544eedc4c25ea345a21
maikerumine <michaellorenger@yahoo.com>
2017-12-04 23:18:21 -0500

    ESM5 Server release

--- mods/shooter/shooter.lua
+++ mods/shooter/shooter.lua
@@ -708,7 +708,7 @@ if not singleplayer and SHOOTER_ADMIN_WEAPONS then
 		end
 	end)
 end
---[[
+
 minetest.register_craft({
 	type = "cooking",
 	cooktime = 60,
@@ -798,4 +798,4 @@ minetest.register_craft({
 	output = "default:steel_ingot 2",
 	recipe = "shooter:crossbow",
 })
-]]
+
