7c6b6c4690a3b0298cd73f53a7423a56557a70f2
maikerumine <michaellorenger@yahoo.com>
2020-12-14 09:23:38 +0000

    redo all code

--- mods/laptop/init.lua
+++
@@ -1,9 +0,0 @@
-laptop = {}
-laptop.class_lib = {}
-dofile(minetest.get_modpath('laptop')..'/themes.lua')
-dofile(minetest.get_modpath('laptop')..'/block_devices.lua')
-dofile(minetest.get_modpath('laptop')..'/app_fw.lua')
-dofile(minetest.get_modpath('laptop')..'/os.lua')
-dofile(minetest.get_modpath('laptop')..'/node_fw.lua')
-dofile(minetest.get_modpath('laptop')..'/nodes.lua')
-dofile(minetest.get_modpath('laptop')..'/items.lua')
