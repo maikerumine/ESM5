c17eecd25c4226668e5a8672a406177f9b8fa34b
maikerumine <michaellorenger@yahoo.com>
2020-12-14 08:45:23 +0000

    REDO ALL DATA for ESM

--- mods/BBQ/init.lua
+++
@@ -1,6 +0,0 @@
-dofile(minetest.get_modpath("bbq").."/cooking.lua")
-dofile(minetest.get_modpath("bbq").."/crafts.lua")
-dofile(minetest.get_modpath("bbq").."/kettle.lua")
-dofile(minetest.get_modpath("bbq").."/smoker.lua")
-dofile(minetest.get_modpath("bbq").."/overrides.lua")
-dofile(minetest.get_modpath("bbq").."/woodpile.lua")
